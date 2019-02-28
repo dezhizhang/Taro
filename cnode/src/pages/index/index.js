ff164d9fd687741de32b15b923647dbb3bbde825
dezhizhang <1541609448@qq.com>
2019-02-28 22:19:44 +0800

    项目初始化

---
+++ cnode/src/pages/index/index.js
@@ -0,0 +1,32 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Button, Text } from '@tarojs/components'
+import './index.less'
+
+
+
+class Index extends Component {
+
+    config = {
+    navigationBarTitleText: '首页'
+  }
+
+  componentWillReceiveProps (nextProps) {
+    console.log(this.props, nextProps)
+  }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <View>hello</View>
+      </View>
+    )
+  }
+}
+
+export default Index
