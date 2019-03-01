7e6325ee70382d9043664753283d24c8b5368c9e
dezhizhang <3142922546@qq.com>
2019-03-01 20:39:15 +0800

    静态页面

--- cnode/src/components/menu/menu.js
+++ cnode/src/components/menu/menu.js
@@ -1,11 +1,20 @@
-import Taro,{ Component } from '@tarojs/taro';
-import { View,Text,Button } from '@tarojs/components';
+import Taro,{ Component, request } from '@tarojs/taro';
+import { View,Text,Button,Image } from '@tarojs/components';
 import { connect } from '@tarojs/redux';
+import cata from '../../assets/imgs/cata.png';
+import login from '../../assets/imgs/login.png'
 import './menu.less'
+
 
 class Menu extends Component {
    render() {
-       return (<View className="topicList-menu">Menu</View>)
+       return (
+        <View className="topicList-menu">
+           <Image src={ cata }/>
+           <Text></Text>
+           <Image src={ login }/>
+        </View>
+       )
    }
 }
 
