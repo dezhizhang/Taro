31bb76aacf7a64a6b30eaee24967ebb0100a4d4d
dezhizhang <1541609448@qq.com>
2019-03-09 00:17:19 +0800

    修改

--- cnode/src/pages/login/login.js
+++ cnode/src/pages/login/login.js
@@ -1,5 +1,5 @@
 import Taro,{ Component } from '@tarojs/taro';
-import { View,Text,Button,Image, } from '@tarojs/components';
+import { View,Text,Button,Image,Input,Swiper,SwiperItem  } from '@tarojs/components';
 import { connect } from '@tarojs/redux';
 import { AtInput } from 'taro-ui'
 import './login.less'
@@ -8,16 +8,30 @@ import loginBg from '../../assets/imgs/bg.png';
 
 class Login extends Component {
     config = {
-        navigationBarTitleText:"天天共享"
+        navigationBarTitleText:"网易严选"
     }
-    render() {               
+    state = {
+        nav:['推荐','新品','众筹','福利']
+    }
+    render() { 
+        let  { nav } = this.state;              
         return (
             <View className="wrapper">
-               <View className="header">
-                    <View className="header-left"></View>
-                    <View className="header-center"></View>
+                <View className="header">
+                    <View className="header-left">
+                        <Text>扫一扫</Text>
+                    </View>
+                    <View className="header-center">
+                        <Input className='header-seach' placeholder="🔍搜索商品，共10628款好物" type='text'/>
+                    </View>
                     <View className="header-right"></View>
-               </View>
+                </View>
+                <View className="nav">
+                    <View className='nav-title'>推荐</View>
+                </View>
+                <View className="banner">
+                    
+                </View>
             </View>
             )
     }
